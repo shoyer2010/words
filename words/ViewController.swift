b464bcec0dbfe25dc3fe7e79af6dc2540ce03ee0
文红军 <wenhongjun@coolhey.cn>
2015-01-29 20:00:02 +0800

    intialize the project

---
+++ words/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  words
+//
+//  Created by shoyer on 15/1/29.
+//  Copyright (c) 2015年 shoyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

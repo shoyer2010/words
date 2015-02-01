091c997583f20abe3672689dda84f94ae1eb2d83
lizheng <120401622@qq.com>
2015-02-01 16:37:33 +0800

    AppDelegate

--- words/AppDelegate.swift
+++ words/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+//        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+//        
+//        let homeController: HomeController = HomeController()
+//        self.window!.rootViewController = homeController
+//        
+//        self.window!.backgroundColor = UIColor.whiteColor()
+//        self.window!.makeKeyAndVisible()
+        
         return true
     }
 

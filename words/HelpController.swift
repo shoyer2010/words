8f9dabca4540237e2347048246ac841cd5b5799c
文红军 <wenhongjun@coolhey.cn>
2015-03-03 01:03:11 +0800

    help page.

--- words/HelpController.swift
+++ words/HelpController.swift
@@ -9,8 +9,11 @@
 import Foundation
 import UIKit
 
-class HelpController: UIViewController, APIDataDelegate {
+class HelpController: UIViewController, APIDataDelegate, UIWebViewDelegate {
     var helpView: UIView!
+    var webView: UIWebView!
+    
+    var indicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,30 +22,26 @@ class HelpController: UIViewController, APIDataDelegate {
         self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0)
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "onTapView:"))
         
-        self.helpView = UIView(frame: CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height))
+        self.helpView = UIView(frame: CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height - 55))
         self.helpView.backgroundColor = Color.red
         self.helpView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: nil)) // prevent tap event from delivering to parent view.
         self.helpView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: nil))
         self.view.addSubview(self.helpView)
         
+        self.webView = UIWebView(frame: CGRect(x: 0, y: 0, width: self.helpView.frame.width, height: self.helpView.frame.height))
+        self.helpView.addSubview(self.webView)
         
-        var button = UIButton(frame: CGRect(x:100, y: 100, width: 100, height: 100))
-        button.backgroundColor = UIColor.whiteColor()
-        button.addTarget(self, action: "onButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        var label = UILabel(frame: CGRect(x:0, y: 0, width: 100, height: 100))
-        label.text = "返回"
-        button.addSubview(label)
-        
-        var helpLabel = UILabel(frame: CGRect(x: 100, y: 300, width: 100, height: 100))
-        helpLabel.text = "帮助页面"
-        self.helpView.addSubview(helpLabel)
-        self.helpView.addSubview(button)
+        indicator = UIActivityIndicatorView(frame: CGRect(x: self.webView.frame.width / 2 - 15, y: self.webView.frame.height / 2 - 15, width: 30, height: 30))
+        indicator.color = Color.red
+        self.webView.addSubview(indicator)
         
         UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
             self.helpView.transform = CGAffineTransformMakeTranslation(0, 55 - self.view.frame.height)
             self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
             }) { (isDone: Bool) -> Void in
+                var request = NSURLRequest(URL: NSURL(string: Server.entry() + "/faq.html")!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: NSTimeInterval(10))
+                self.webView.delegate = self
+                self.webView.loadRequest(request)
         }
     }
     
@@ -62,5 +61,21 @@ class HelpController: UIViewController, APIDataDelegate {
                 self.view.removeFromSuperview()
                 self.removeFromParentViewController()
         }
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        self.webView.bringSubviewToFront(self.indicator)
+        self.indicator.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        self.indicator.stopAnimating()
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        self.indicator.stopAnimating()
     }
 }

25304addd5bcbafe0fb47336ea991afae92bee9a
文红军 <wenhongjun@coolhey.cn>
2015-03-09 02:31:40 +0800

    service info.

--- words/ServiceInfoController.swift
+++ words/ServiceInfoController.swift
@@ -1,18 +1,10 @@
-//
-//  HelpController.swift
-//  words
-//
-//  Created by shoyer on 15/2/3.
-//  Copyright (c) 2015年 shoyer. All rights reserved.
-//
 
 import Foundation
 import UIKit
 
-class HelpController: UIViewController, APIDataDelegate, UIWebViewDelegate {
-    var helpView: UIView!
+class ServiceInfoController: UIViewController, APIDataDelegate, UIWebViewDelegate {
+    var subView: UIView!
     var webView: UIWebView!
-    
     var indicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
@@ -21,29 +13,30 @@ class HelpController: UIViewController, APIDataDelegate, UIWebViewDelegate {
         
         self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0)
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "onTapView:"))
+        var subViewHeight: CGFloat = self.view.frame.height - 120
         
-        self.helpView = UIView(frame: CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height - 55))
-        self.helpView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: nil)) // prevent tap event from delivering to parent view.
-        self.helpView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: nil))
-        self.helpView.backgroundColor = Color.white.colorWithAlphaComponent(0.89)
-        self.view.addSubview(self.helpView)
+        self.subView = UIView(frame: CGRect(x: 0, y: -subViewHeight, width: self.view.frame.width, height: subViewHeight))
+        self.subView.backgroundColor = Color.white.colorWithAlphaComponent(0.89)
+        self.subView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: nil)) // prevent tap event from delivering to parent view.
+        self.subView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: nil))
+        self.view.addSubview(self.subView)
         
-        self.webView = UIWebView(frame: CGRect(x: 0, y: 0, width: self.helpView.frame.width, height: self.helpView.frame.height))
+        self.webView = UIWebView(frame: CGRect(x: 0, y: 0, width: self.subView.frame.width, height: self.subView.frame.height))
         self.webView.backgroundColor = UIColor.clearColor()
-        self.helpView.addSubview(self.webView)
+        self.subView.addSubview(self.webView)
         
         indicator = UIActivityIndicatorView(frame: CGRect(x: self.webView.frame.width / 2 - 15, y: self.webView.frame.height / 2 - 15, width: 30, height: 30))
         indicator.color = Color.red
         self.webView.addSubview(indicator)
         
         UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
-            self.helpView.transform = CGAffineTransformMakeTranslation(0, 55 - self.view.frame.height)
+            self.subView.transform = CGAffineTransformMakeTranslation(0, subViewHeight)
             self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
             }) { (isDone: Bool) -> Void in
                 var user: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey(CacheKey.USER)
                 var userId = user?["id"] as? NSString
                 
-                var request = NSURLRequest(URL: NSURL(string: Server.entry() + "/user/faq?userId=\(userId!)")!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: NSTimeInterval(10))
+                var request = NSURLRequest(URL: NSURL(string: Server.entry() + "/user/serviceInfo?userId=\(userId!)")!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: NSTimeInterval(10))
                 self.webView.delegate = self
                 self.webView.loadRequest(request)
         }
@@ -57,9 +50,13 @@ class HelpController: UIViewController, APIDataDelegate, UIWebViewDelegate {
         self.closeView()
     }
     
+    func onCancelTapped(sender: UIButton) {
+        self.closeView()
+    }
+    
     func closeView() {
         UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
-            self.helpView.transform = CGAffineTransformMakeTranslation(0, self.view.frame.height - 55)
+            self.subView.transform = CGAffineTransformMakeTranslation(0, -(self.view.frame.height - 120))
             self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0)
             }) { (isDone: Bool) -> Void in
                 self.view.removeFromSuperview()

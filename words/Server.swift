e5db8b7fde453d1cf2abda720893476e11e26346
文红军 <wenhongjun@coolhey.cn>
2015-03-20 16:11:28 +0800

    use ip as sever entry.

--- words/Server.swift
+++ words/Server.swift
@@ -10,7 +10,8 @@ import Foundation
 
 struct Server {
 //    static let host = "dev.coolhey.cn"
-    static let host = "server.wordholy.com"
+//    static let host = "server.wordholy.com"
+      static let host = "121.43.228.158" // TODO: 这一版先用ip访问，等下一版域名备案下来，用域名访问
 //    static let host = "localhost"
     static let port = "1337"
     
